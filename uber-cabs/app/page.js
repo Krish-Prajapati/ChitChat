98e99740358d9107141997008caf03ffdd0e83d4
KrishPrajapati <krrish.s.prajapati@gmail.com>
2024-09-29 15:48:59 +0000

    user login and logout implementation

--- uber-cabs/app/page.js
+++ uber-cabs/app/page.js
@@ -1,9 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      HomePage
+      {/* <UserButton/> */}    
+      {/* .->userbutton was already defined in (layout.js) */}
     </div>
   );
 }
--- uber-cabs/app/sign-in/[[...sign-in]]/page.js
+++ uber-cabs/app/sign-in/[[...sign-in]]/page.js
@@ -1,5 +1,16 @@
 import { SignIn } from '@clerk/nextjs'
+import Image from 'next/image';
 
 export default function Page() {
-  return <SignIn />
+  return (
+    <>
+    <div>
+      <Image src="/IMG_20240507_001136_131.jpg" width={1000} height={500}
+      className="object-contain h-full w-full"/>
+      <div className="absolute top-10 right-0">
+        <SignIn/>
+      </div>
+    </div>
+    </>
+);
 }
